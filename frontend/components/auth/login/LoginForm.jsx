37e17dea74b03b19e1405e6f8603dc5d1a9bc321
suphanatchanlek <suphanatchanlek@gmail.com>
2025-10-05 13:47:22 +0700

    feat(fe/login-register): add UI For Page Register

---
+++ frontend/components/auth/login/LoginForm.jsx
@@ -0,0 +1,74 @@
+// components/auth/login/LoginForm.jsx
+
+"use client";
+
+// ฟอร์ม Login ที่จะเชื่อม API จริงในภายหลัง
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: เรียก API Login ของ Backend ที่คุณทำไว้ เช่น /api/auth/login
+    console.log({ email, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      {/* ช่องกรอก Email */}
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          E-mail address
+        </label>
+        <input
+          type="email"
+          className="w-full mt-1 p-2 border-1 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+          placeholder="example@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      {/* ช่องกรอก Password */}
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Password
+        </label>
+        <input
+          type="password"
+          className="w-full mt-1 p-2 border-1 border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-gray-400"
+          placeholder="Your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        
+        {/* ลืมรหัสผ่าน */}
+        {/* <div className="text-right mt-1">
+          <a href="#" className="text-sm text-[#1800ad] hover:underline">
+            Lost your password?
+          </a>
+        </div> */}
+      </div>
+
+      {/* ปุ่ม Login */}
+      <button
+        type="submit"
+        className="w-full bg-[#1800ad] hover:bg-[#1F0C48FF] text-white py-2 rounded-lg text-lg font-medium mt-4"
+      >
+        Sign In
+      </button>
+
+      {/* ลิงก์ไปสมัครสมาชิก */}
+      <p className="text-center text-sm text-gray-600 mt-2">
+        New account?{" "}
+        <a href="/register" className="text-[#1800ad] hover:underline">
+          Register here.
+        </a>
+      </p>
+    </form>
+  );
+}
--- frontend/components/auth/register/RegisterSection.jsx
+++ frontend/components/auth/register/RegisterSection.jsx
@@ -1,6 +1,4 @@
-// app/(auth)/register/page.jsx
-
-export default function RegisterPage() {
+export default function RegisterSection() {
     // ดึงเป็น components มาใช้
     return <h1>RegisterPage</h1>;
 }
