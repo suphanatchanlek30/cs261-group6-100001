e93ef873c3eed5283d3cbc24a4aca9c0383f971f
suphanatchanlek <suphanatchanlek@gmail.com>
2025-10-09 01:41:07 +0700

    Fix(fe): Fix admin-dashboard

--- frontend/app/(auth)/login/page.jsx
+++ frontend/app/(auth)/login/page.jsx
@@ -1,9 +1,12 @@
 // app/(auth)/login/page.jsx
 
+import React, { Suspense } from "react";
 import LoginSection from "@/components/auth/login/LoginSection";
 
 export default function LoginPage() {
     return (
-        <LoginSection/>
+        <Suspense fallback={<div />}>
+            <LoginSection />
+        </Suspense>
     );
 }
