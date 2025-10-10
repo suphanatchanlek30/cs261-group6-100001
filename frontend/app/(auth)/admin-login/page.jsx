1a78e76f6e2b34f6010d7a772f921abf44b95d17
suphanatchanlek <suphanatchanlek@gmail.com>
2025-10-10 13:18:20 +0700

    Fix Bug

--- frontend/app/(auth)/admin-login/page.jsx
+++ frontend/app/(auth)/admin-login/page.jsx
@@ -1,7 +1,12 @@
 // app/(auth)/admin-login/page.jsx
 
+import React, { Suspense } from "react";
 import AdminSection from "@/components/auth/admin/AdminSection";
 
 export default function AdminLoginPage() {
-  return <AdminSection />;
+  return (
+    <Suspense fallback={<div />}>
+      <AdminSection />
+    </Suspense>
+  );
 }
