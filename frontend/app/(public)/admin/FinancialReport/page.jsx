0a7998aa7b2578550e2b9b0ba384b979206e92b1
chalisa <chalisa7349@gmail.com>
2025-11-07 23:56:30 +0700

    feat(fe): add card total booking

--- frontend/app/(public)/admin/FinancialReport/page.jsx
+++ frontend/app/(public)/admin/FinancialReport/page.jsx
@@ -1,5 +1,6 @@
 // app/(public)/host/FinancialReport/page.jsx
 "use client";
+import FinancialReportContent from "@/components/admin-dashboard/FinancialReport/financialcontent";
 
 export default function AdminFinancialReportPage() {
     return (
@@ -8,6 +9,7 @@ export default function AdminFinancialReportPage() {
                 <h1 className="text-2xl font-bold text-gray-800">Summary Financial Report</h1>
                 <p className="mt-4 text-gray-600">ดูรายงานสรุปทางการเงินได้ที่นี่ !</p>
             </section >
+            <FinancialReportContent />
         </main >
     );
 }
