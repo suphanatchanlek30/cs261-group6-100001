b64efa6dd7e6f28f33da593283659ce323aa6d8d
chalisa <chalisa7349@gmail.com>
2025-11-07 21:46:43 +0700

    feat(fe): add financial report page for admin dashboard

--- frontend/app/(public)/admin/FinancialReport/page.jsx
+++ frontend/app/(public)/admin/FinancialReport/page.jsx
@@ -1,16 +1,12 @@
 // app/(public)/host/FinancialReport/page.jsx
 "use client";
 
-export default function FinancialReportPage() {
+export default function AdminFinancialReportPage() {
     return (
         <main>
             < section className="mb-8" >
-                <h1 className="text-2xl font-bold text-gray-800">
-                    Summary Financial Report
-                </h1>
-                <p className="mt-4 text-gray-600">
-                    ดูรายงานสรุปทางการเงินได้ที่นี่ !
-                </p>
+                <h1 className="text-2xl font-bold text-gray-800">Summary Financial Report</h1>
+                <p className="mt-4 text-gray-600">ดูรายงานสรุปทางการเงินได้ที่นี่ !</p>
             </section >
         </main >
     );
